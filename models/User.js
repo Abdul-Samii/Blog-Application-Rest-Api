3b4bb9fb116937766655bb2c2245c86561f603cb
Abdul-Samii <ksamk100474@gmail.com>
2022-01-15 19:02:40 +0500

    User Register API

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        requierd:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        requied:true
+    },
+    profilePic:{
+        type:String,
+        default:""
+    },
+},
+    {timestamps:true}
+);
+
+module.exports = mongoose.model("User",UserSchema);
